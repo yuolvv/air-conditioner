090fe12a7d3ebd539be157b1ed09e86c3828f4d7
Yuol Cheung <yuolvv@gmail.com>
2021-05-19 11:37:47 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,12 @@ import { useEffect } from "react";
 function App() {
   useEffect(() => {
     console.log(
-      `%c ☁️ air-conditioner %c https://github.com/YunYouJun/air-conditioner`,
+      `%c ☁️ air-conditioner %c https://github.com/yuolvv/air-conditioner`,
       "color: white; background: #0078E7; padding:5px 0;",
       "padding:4px;border:1px solid #0078E7;"
     );
     console.log(
-      `%c ☁️ @YunYouJun %c https://www.yunyoujun.cn`,
+      `%c ☁️ @听风行 %c https://air.iyu.pub`,
       "color: white; background: #0078E7; padding:5px 0;",
       "padding:4px;border:1px solid #0078E7;"
     );
